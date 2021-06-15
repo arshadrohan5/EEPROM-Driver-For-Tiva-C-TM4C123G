84566fd5a9a5db97c0763cd90df525afbb3dc20b
Rohan Arshid <arshadrohan5@gmail.com>
2021-06-15 18:33:25 +0500

    Update eeprom.h

--- EEPROM Driver/eeprom.h
+++ EEPROM Driver/eeprom.h
@@ -2,7 +2,7 @@
 #define EEPROM_H
 #include "tm4c123gh6pm.h"
 void eeprom_init(void);
-void eeprom_write(int data,uint8_t addr);
-int eeprom_read(uint8_t addr);
+void eeprom_write(int data,uint8_t addr,uint8_t blk);
+int eeprom_read(uint8_t addr,uint8_t blk);
 
 #endif
